4dede6c8d1668c720b787e37f2e77823613184b4
Gleb Malkin <54169797+GlebMalkin@users.noreply.github.com>
2024-12-20 06:05:21 +0300

    Delete kotlin_1mg/kotlin_1pract-master directory

--- kotlin_1mg/kotlin_1pract-master/src/Expense.kt
+++
@@ -1,5 +0,0 @@
-data class Expense(val amount: Double, val category: String, val date: String) {
-    fun displayExpense() {
-        println("Количество: $amount, Категория: $category, Дата: $date")
-    }
-}
